967a5159bce9ecae9b5d9f9aa165a0593bb25184
Victor Tram <victortram1996@gmail.com>
2019-09-03 00:06:47 -0700

    Requirements steps 1 & 2

--- challengeCalculator/challengeCalculator.cs
+++ challengeCalculator/challengeCalculator.cs
@@ -7,33 +7,48 @@ public class challengeCalculator{
 // Use a comma delimited format e.g. 5000 will return 5000; 1,20 will return 21
 // Invalid/Missing numbers should be converted to 0 e.g. "" will return 0; 5,tytyt will return 5
 	public static void Main(string[] args){
-		//System.Console.WriteLine("Test");
+		System.Console.WriteLine("Test");
         string subjectString;
-		Console.Write("Enter String");
+		Console.Write("Enter String\n");
 		subjectString = Console.ReadLine();
 
-
-        string res = calculator(subjectString);
-        System.Console.WriteLine(res);
-    }
-
-    public static string calculator(string str){
+        
         
 
-        int[] nums = getVals(str);
-        return "test";
+        int sum = calculator(subjectString);
+        System.Console.WriteLine(sum);
+    }
+
+    public static int calculator(string str){
+        
+
+        //int[] nums = getVals(str);
+        int sum=0;
+        foreach(int element in getVals(str)){
+            sum = sum + element;
+        }
+        return sum;
     }
 
     public static int[] getVals(string str){
-        string delimiter = ",";
-        
-        //int[] strlist = int.Parse(str.Split(delimiter));
-        //foreach (int element in strList)
-        //{
+        char delimiter = ',';
+        String[] strList = str.Split(delimiter);
+        int[] intlist = new int[strList.Length];
+        int count = 0;
+        int number;
+        foreach (string element in strList)
+        {
+            
+            bool res = int.TryParse(element, out number);
+            intlist[count] = number;
             //Console.WriteLine($"Element: {element}");
-        //}
-        int[] res = {1,2,3};
-        return res;
+            count++;
+        }
+        //int[] res = {1,2,3};
+
+
+
+        return intlist;
     }
 
 }
